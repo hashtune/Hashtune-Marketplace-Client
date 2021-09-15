8357ac90d707adb74989e70553bb7f6a0e8b0106
Alex <alex.alber.aa@gmail.com>
2021-09-15 19:08:46 +0200

    feat: Added Basic Navbar Components

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <nav className= "NavbarItems">
+      <h1 className = "navbar-logo">Hashtune</h1>
+      <div className= "menu-icon"></div>
+      <ul>
+        <li><a href=""></a></li>
+      </ul>
+    </nav>
+  )
+}
