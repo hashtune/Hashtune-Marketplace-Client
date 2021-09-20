4c800bb052b8f997a2c7cf37ac92700099456676
Alex <alex.alber.aa@gmail.com>
2021-09-20 23:17:06 +0200

    Added Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,23 @@
 import Link from 'next/link'
+import styles from '.../css/Navbar.module.scss'
+import Image from 'next/image'
 export const Navbar = () => {
   return (
-    <nav className= "NavbarItems">
+    <>
+    <div className = {styles.mainSidebar}>
+    <div className= {styles.sidebar}>
+    <nav className= {styles.navbar}>
       <ul>
-        <li><Link href= '/'><a>Hashtune</a></Link></li>
-        <form><input type="text" id="search" name="search"/></form>
-        <li><Link href= '/explore' ><a>Explore</a></Link></li>
+        <li><Image src="/images/logo.png" alt="me" width="18.75" height="17.5" /><Link href= '/'><a>Hashtune</a></Link></li>
+        <li><Link href= '/explore' ><a>Home</a></Link></li>
+        <li><Link href= '/home'><a>Connect Wallet</a></Link></li>
         <li><Link href= '/blog' ><a>Blog</a></Link></li>
         <li><Link href= '/about' ><a>About</a></Link></li>
         <li><Link href= '/connect-wallet'><a>Connect Wallet</a></Link></li>
       </ul>
     </nav>
+    </div>
+    </div>
+    </>
   )
 }
