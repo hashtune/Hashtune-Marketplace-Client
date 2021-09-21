a6fdc2038a46444f33e5ad02048de38b46e570c3
Alex <alex.alber.aa@gmail.com>
2021-09-21 23:40:51 +0200

    feat: Added new methods for dynamic routing (Need to able to fetch artworks by id to complete this)

--- pages/api/artwork/[id]/index.ts
+++ pages/api/artwork/[id]/index.ts
@@ -1,46 +1,68 @@
 import { gql } from "@apollo/client";
 import { resultKeyNameFromField } from "@apollo/client/utilities";
 import { GetStaticPaths, GetStaticProps, NextApiRequest, NextApiResponse } from "next";
+import Head from "next/head";
 import client from "../../../../apollo-client";
+import Layout from "../../../../components/layout";
 
-export const getStaticProps:  GetStaticProps = async() => {
-  const {data} = await client.query({
-      query: gql`
-        query ExampleQuery {
-          listArtworks {
-            id
-          }
+const getAllArtworksData = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query ExampleQuery {
+        listArtworks {
+          id
         }
-      `,
-    });
-    return {
-      props: {
-        allArtworksData: data.listArtworks.slice(0, 8),
-        fallback: true
-      },
-   };
+      }
+    `,
+  });
+  return data.listArtworks.slice(0,8);
+};
+export const getStaticProps: GetStaticProps = async () => {
+  const allArtworksData = await getAllArtworksData();
+  return {
+    props: {
+      allArtworksData: allArtworksData,
+      fallback: true
+    },
+  };
 }
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllArtworkIds()
+  const allArtworksData = await getAllArtworksData();
+  const paths = getAllArtworkIds(allArtworksData)
   return {
     paths,
     fallback: true
   }
 }
-
-
-
-export default function getAllArtworkIds({allArtworksData} : {
-    allArtworksData: {
-        id: string
-    }[]
-} ){
-    const allArtworkIds = allArtworksData.map(artwork => artwork.id )
-    return allArtworkIds.map(artworkId => {
-        return {
-            params: {
-                id: artworkId
-            }
-        }
-    })
+export function getAllArtworkIds({ allArtworksData }: {
+  allArtworksData: {
+    id: string
+  }[]
+}) {
+  const allArtworkIds = allArtworksData?.map(artwork => artwork.id)
+  console.log(allArtworkIds)
+  return allArtworkIds?.map(artworkId => {
+    return {
+      params: {
+        id: artworkId
+      }
+    }
+  })
 }
+
+export async function getArtworkById(id: string) {
+    const { data } = await client.query({
+    query: gql`
+      query ExampleQuery {
+        listArtworks(id : ${id}) {
+          id
+        }
+      }
+    `,
+  });
+}
+// USE LATER POTENTIALLY
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   // Get data from your database
+//   res.status(200).json(getAllArtworkIds)
+// }
