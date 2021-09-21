14299a52694aa19f8e73d46b5890afa9ba572acb
Alex <alex.alber.aa@gmail.com>
2021-09-21 23:33:47 +0200

    feat: Added MenuItems to the Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
-import styles from '.../css/Navbar.module.scss'
+import styles from '../../css/Navbar.module.scss'
 import Image from 'next/image'
+import MenuItem from './MenuItem'
 export const Navbar = () => {
   return (
     <>
@@ -8,12 +9,12 @@ export const Navbar = () => {
     <div className= {styles.sidebar}>
     <nav className= {styles.navbar}>
       <ul>
-        <li><Image src="/images/logo.png" alt="me" width="18.75" height="17.5" /><Link href= '/'><a>Hashtune</a></Link></li>
-        <li><Link href= '/explore' ><a>Home</a></Link></li>
-        <li><Link href= '/home'><a>Connect Wallet</a></Link></li>
-        <li><Link href= '/blog' ><a>Blog</a></Link></li>
-        <li><Link href= '/about' ><a>About</a></Link></li>
-        <li><Link href= '/connect-wallet'><a>Connect Wallet</a></Link></li>
+        <li className= "logo"><MenuItem name= "Hashtune" href = '/' iconsrc= "/images/logo.png"/></li>
+        <li><MenuItem name= "Home" href= '/home' iconsrc= '/images/ion_home-outline.png'/></li>
+        <li><MenuItem name= "Explore" href= '/explore' iconsrc= '/images/ion_compass-outline.png'/></li>
+        <li><MenuItem name= "Blog" href= '/blog' iconsrc= '/images/ion_document-text-outline.png'/></li>
+        <li><MenuItem name= "About" href= '/about' iconsrc= '/images/ion_glasses.png'/></li>
+        <li><MenuItem name= "Connect Wallet" href= '/connect-wallet' iconsrc=''/></li>
       </ul>
     </nav>
     </div>
