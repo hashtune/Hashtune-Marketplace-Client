e8ecbdeb3759765227cea38e3d8ee5bfe6e06dba
Alex <alex.alber.aa@gmail.com>
2021-09-23 00:19:50 +0200

    feat: Added FollowButton component

---
+++ components/Home/ListCreator/FollowButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FollowButton = () => {
+    return (
+        <div>
+            <div>
+                <button>
+                    <div>Follow</div>
+                </button>
+            </div>
+        </div>
+    )
+}
+export default FollowButton
