89b6fccd92ac2d71ff677655ef16d55fbdd0f029
Alex <alex.alber.aa@gmail.com>
2021-09-23 00:10:12 +0200

    feat: Added Bit Button component for Hero

---
+++ components/Home/Hero/BidButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BidButton = () => {
+    return (
+        <div>
+            <div>
+                <button>
+                    <div>Bid</div>
+                </button>
+            </div>
+        </div>
+    )
+}
+export default BidButton
