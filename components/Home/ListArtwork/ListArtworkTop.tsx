fc638d88a4142e37d2943b897878d2f24a0b302a
Alex <alex.alber.aa@gmail.com>
2021-09-23 00:12:30 +0200

    feat: Added ListArtworkTop component

--- components/Home/ListArtwork/ListArtworkTop.tsx
+++ components/Home/ListArtwork/ListArtworkTop.tsx
@@ -1,31 +1,40 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image"
+import CreatorImageHandle from "../ListArtist/ArtistIconHandle";
 
 interface ListArtworkProps {
   name: string,
-  coversrc: string,
+  image: string,
+  genre: string[],
+  artworkType: string,
+  artistImage: string,
   artistHandle: string,
-  genre: string,
-  type: string
 }
 
 
+
+
 const ListArtworkTop = (props: ListArtworkProps) => {
+    const href: string ='../../../pages/api/artist/';
     return (
         <div>
-            <Image src= {props.coversrc} width = {206} height= {206}/>
+            <Image src= {props.image} width = {206} height= {206}/>
             <div className="artwork - info">
                 <div>
                     <div className = "type-genre">
                         <div className="name">
                             <Link href= '/'>{props.name}</Link>
                         </div>
-                        <div className ="genre">
-                            <Link href= '/'>{props.genre}</Link>
-                        </div>
-                        <div className ="type">
-                            <Link href= '/'>{props.type}</Link>
+                        <CreatorImageHandle image={props.artistImage} handle={props.artistHandle} />
+                        <ul className ="genre">
+                            {props.genre.map ( genre => (
+                                <li>#{genre}</li>
+                                )
+                            )}
+                        </ul>
+                        <div className ="artworkType">
+                            <Link href= '/'>{props.artworkType}</Link>
                         </div>
                     </div>
                 </div>
