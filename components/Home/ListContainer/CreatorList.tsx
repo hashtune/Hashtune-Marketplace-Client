c09e84e7895e9be937cabffdfdba1c7a9a280cda
Alex <alex.alber.aa@gmail.com>
2021-09-23 00:18:51 +0200

    feat: Added CreatorList component

---
+++ components/Home/ListContainer/CreatorList.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import ListArtist from "../ListArtist/ListArtist"
+import CreatorListRow from "./CreatorListRow"
+import TitleViewAll from "./TitleViewAll"
+
+interface CreatorListProps {
+    title: string,
+    creators: {
+        id: string,
+        name: string
+        handle: string,
+        image: string,
+        bio:string
+    }[]
+}
+
+const CreatorList = (props: CreatorListProps) => {
+    return (
+        <div>
+            <div>
+                <TitleViewAll title={props.title} viewAll={"Creators"}/>
+                <div>
+                    <CreatorListRow start={0} end={4} creators={props.creators}/>
+                    <CreatorListRow start={4} end={8} creators={props.creators}/>
+                    <CreatorListRow start={8} end={12} creators={props.creators}/>
+                    <CreatorListRow start={12} end={16} creators={props.creators}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CreatorList
