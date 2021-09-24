125cd12959f2e7e8f2fcfddb83dd877fe46bf5b7
Alex <alex.alber.aa@gmail.com>
2021-09-24 15:58:52 +0200

    refactor: List Artwork now contains two reusable interface for components that need Artwork as prop

--- components/Home/ListArtwork/ListArtwork.tsx
+++ components/Home/ListArtwork/ListArtwork.tsx
@@ -1,33 +1,53 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image"
-import ListArtworkTop from "./ListArtworkTop";
-import ListArtworkBottom from "./ListArtworkBottom";
+import ArtworkTitleCreator from "./ArtworkTitleCreator";
+import Price from "./Price";
 
-interface ListArtworkProps {
-  name: string,
-  coverImage: string,
-  creatorHandle: string,
-  genre: string [],
-  artworkType: string
-  creatorImage: string
-
-  purchasable?: boolean,
-  auction?: boolean,
-  price?: number,
-  inStock?: number,
-  currentBid?: number,
-  endingIn?: string,
+export interface Artwork {
+    id: string,
+    title: string
+    handle: string,
+    image?: string,
+    artworkType: string,
+    saleType: string,
+    currentBid?: number,
+    fixedPrice: number,
+    reservePrice: number,
+    liveAt?: Date,
+    creators: {
+       handle: string,
+       name: string,
+       image: string 
+    }[],
+}
+export interface ArtworkProp {
+    artwork: Artwork
 }
 
-
-
-const ListArtwork = (props: ListArtworkProps) => {
+const ListArtwork = (props: ArtworkProp) => {
+    const artwork = props.artwork;
+    const coverImage = artwork.image || "/";
     return (
         <div>
             <div>
-                <ListArtworkTop name={props.name} image={props.coverImage} creatorHandle={props.creatorHandle} genre={props.genre} artworkType={props.artworkType} creatorImage={props.creatorImage}/>
-                <ListArtworkBottom auction= {props.auction} purchasable = {props.purchasable} inStock= {props.inStock} currentBid= {props.currentBid}endingIn={props.endingIn}/>
+                <div>
+                    <Image src= {coverImage}/>
+                </div>
+                <div>
+                    <ArtworkTitleCreator artwork = {artwork}/> 
+                    <Price artwork= {artwork}/>
+                </div>
+                
+                {/* Listed == false Show artwokrk creator
+                If auctionWithNoReservePriceAndNoBids -> Make an offer
+                If saleType == fixed Buy {price}
+                if latestAuction.bids == [] --> Bid {artwork.reservePrice}
+                else {
+                    Bid {currentHigh} BNB
+                }
+                
+                */}
             </div>
         </div>
     )
