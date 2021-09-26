3949bfa6b02179c2138eb6451f8584094751ce20
Alex <alex.alber.aa@gmail.com>
2021-09-26 15:08:52 +0200

    feat: Updated logic in Price component

--- components/Home/ListArtwork/Price.tsx
+++ components/Home/ListArtwork/Price.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Countdown from '../Hero/Countdown'
 import {ArtworkProp} from "./ListArtwork"
@@ -5,28 +6,24 @@ import {ArtworkProp} from "./ListArtwork"
 
 const Price = (props: ArtworkProp) => {
     const artwork = props.artwork
-    if (artwork.saleType=== "auction") {
-        if (artwork.currentBid !== null){
-            return (
-                <div>
-                    <h3>Bid {artwork.currentBid} BNB</h3>
-                </div>
-            )
-        } 
-        else {
-            return (
-            <div>  
-                <h3>Make an offer</h3>
-            </div>
-            )
-        }
-    }
-    else{
-        return (
-            <div>
-                <h3>Buy for {artwork.fixedPrice} BNB</h3>
-            </div>
-    )
+    if (!artwork.listed){
+        return;
+    } else if (artwork.saleType === "fixed"){
+        return( <div>
+                    <h3>Buy for {artwork.price} BNB</h3>
+                </div>)
+    } else if (artwork.auctionWithNoReservePriceAndNoBids){
+        return ( <div>
+                    <h3>Make an offer</h3>
+                </div>)
+    } else if (artwork.latestAuction.bids=== []){
+        return (<div>
+                    <h3>Bid {artwork.reservePrice} BNB</h3>
+                </div>)
+    } else {
+        return (<div>
+                    <h3>Bid {artwork.reservePrice} BNB</h3>
+                </div>)
     }
 }
 export default Price
