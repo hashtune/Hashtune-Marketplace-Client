69cf5b2b9b9c0f73c7f60d4a92d1eeedaaf296c1
Alex <alex.alber.aa@gmail.com>
2021-09-28 00:46:46 +0200

    feat: Changed Hero to new requirements, still missing fields so it doesn't compile

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,38 +4,51 @@ import CreatorIconHandle from "../ListCreator/CreatorIconHandle";
 import BidButton from "./BidButton";
 import Countdown from "./Countdown";
 import {ArtworkProp } from "../ListArtwork/ListArtwork";
+import { GetServerSideProps } from "next";
+import ConvertedPrice, { Coin } from "./ConvertedPrice";
 
-// TODO: REMOVE HARDCODED BACKGROUND IMAGE
-const Hero = (props: ArtworkProp) => {
+
+export const getServerSideProps: GetServerSideProps = async () =>{
+    const res = await fetch(`https://api.coingecko.com/api/v3/coins/binancecoin
+    `);
+    const data = await res.json();
+    return {
+        props: {
+            coin: data
+        }
+    };
+}
+
+const Hero = (props: ArtworkProp, coin: Coin) => {
     let artwork = props.artwork;
     let cover = artwork.image || "/";
     return (
         <div>
-            <Image src= {'/images/backgroundHome.jpg'} width = {1176} height= {640}/>
             <div className= "song-border">
+                <div className= "play-button">
+                    {/* STILL NEED TO IMPORT CORRECT PLAY BUTTON */}
+                    <Image src= "/images/ion_play-circle.png" width={58.5} height={58.5}/>
+                </div>
                 <Image src= {cover} width = {424} height= {424}/>
                 <div className= "song-info">
                     <section className= "song-header">
                         <div className= "title-handle">
-                            <CreatorIconHandle image={artwork.creators[0].image} handle={artwork.creators[0].handle}/>
+                            <CreatorIconHandle image={artwork.creator.image} handle={artwork.creator.handle}/>
                             <h1>{artwork.title}</h1>
+                            <Image src= '/'/> {/* Add small dot in between- export from zeplin*/}
+                            <h1>{artwork.artworkType}</h1>
                         </div>
-                        <div className= "play-button">
-                            <div>
-                                <Image src= "/images/ion_play-circle.png" width={58.5} height={58.5}/>
-                            </div>
+                        <div>
+                            <p>{artwork.description}</p>
                         </div>
                     </section>
                     <section className= "auction-info">
                         <div>
-                            <section>
-                                <h3>Current bid</h3>
-                                <h3>{artwork.currentBid}</h3>
-                            </section>
+                            <ConvertedPrice currentHighInBnb={0} coin={coin}/>
                             <Countdown liveAt= {artwork.liveAt} /> 
                         </div>
-                        <BidButton/>
                     </section>
+                    <BidButton/>
                 </div>
             </div>
         </div>
