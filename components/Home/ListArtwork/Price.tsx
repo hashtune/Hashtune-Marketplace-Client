d24458ef2647747a1232fb2fb2400ca32ff9ae7e
Alex <alex.alber.aa@gmail.com>
2021-09-28 00:48:40 +0200

    feat: added temporary fix for not having auctionWIthNoReservePriceAndNoBids

--- components/Home/ListArtwork/Price.tsx
+++ components/Home/ListArtwork/Price.tsx
@@ -5,25 +5,31 @@ import {ArtworkProp} from "./ListArtwork"
 
 
 const Price = (props: ArtworkProp) => {
-    const artwork = props.artwork
+    const artwork = props.artwork;
     if (!artwork.listed){
         return(<div></div>);
     } else if (artwork.saleType === "fixed"){
         return( <div>
                     <h3>Buy for {artwork.price} BNB</h3>
                 </div>)
-    } else if (artwork.auctionWithNoReservePriceAndNoBids){
+             
+    } 
+    //Should be auctionWithNoReservePriceAndNoBids   
+    // const auctionLength: number = artwork.auctions.length;
+    // Should use last added auction
+    else if (artwork.saleType=== "auction" && artwork.reservePrice===null && artwork.auctions[0].bids === []){
         return ( <div>
                     <h3>Make an offer</h3>
                 </div>)
     } else if (artwork.latestAuction.bids=== []){
         return (<div>
-                    <h3>Bid {artwork.reservePrice} BNB</h3>
+                    <h3>Bid {artwork.auctions[0].currentHigh} BNB</h3>
                 </div>)
     } else {
         return (<div>
                     <h3>Bid {artwork.reservePrice} BNB</h3>
-                </div>)
+                </div>) 
     }
+    // TODO remove reserve price for currentHIgh
 }
 export default Price
