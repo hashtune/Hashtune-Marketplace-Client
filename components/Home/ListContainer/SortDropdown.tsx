fec2ea9fe2062d9d968a3ba9f5774f6818958440
Alex <alex.alber.aa@gmail.com>
2021-09-28 00:50:25 +0200

    feat: Added new SortDropDown component

---
+++ components/Home/ListContainer/SortDropdown.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const SortDropDown = () => {
+    return (
+        <select>
+            <option value="latest">Latest</option>
+            <option value="earliest">earliest</option>
+        </select>
+    )
+}
+export default SortDropDown
