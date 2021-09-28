74ed97eebd493aa1b525137c1d24943e62777db4
Alex <alex.alber.aa@gmail.com>
2021-09-28 12:30:57 +0200

    feat: Added boilerplate Search component

---
+++ components/Layout/Navbar/Search.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image"
+
+const Search = () => {
+    return (
+        <div>
+            <Image src= '/'/>
+            <form>Search</form>
+        </div>
+    )
+}
+export default Search
