1673b387e20ae1244c62a29b4eaa0490e75f620d
Alex <alex.alber.aa@gmail.com>
2021-09-28 12:31:56 +0200

    fix: Temporarily commented out the infinite scroll component in ArtworkContainer

--- components/Home/ListContainer/ArtworkContainer.tsx
+++ components/Home/ListContainer/ArtworkContainer.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import { ArtworkProp } from "../ListArtwork/ListArtwork";
 import InfiniteScroll from "react-infinite-scroll-component";
-import fetchData from '../../../lib/artworks'
-import ListArtwork from "../ListArtwork/ListArtwork";
+// import fetchData from '../../../lib/artworks'
+import ListArtwork, {Artwork} from "../ListArtwork/ListArtwork";
 import Tab from '../../Layout/Navbar/Tab'
 import { useRouter } from "next/dist/client/router";
 
-const ArtworkContainer = (props: ArtworkProp[]) => {
+interface ArtworksProp{
+    artworks: Artwork[],
+}
+
+const ArtworkContainer = (props: ArtworksProp) => {
 const { query } = useRouter();
   const isAllHashtunesSelected = !!query.allHashtunes;
   const isAuctionsSelected = !!query.auctions;
   const isBuyNowSelected = !!query.buyNow;
+
     return (
         <div>
-            <Tab href={""} isSelected={false} title={""}/>
             <Tab href="/?allHashtunes=true" title="All Hashtunes" isSelected={isAllHashtunesSelected} /> 
             <Tab href="/?auctions=true" title="Auctions" isSelected={isAuctionsSelected} />
             <Tab href="/?buyNow=true" title="Buy Now" isSelected={isBuyNowSelected} /> 
-            <InfiniteScroll next={fetchData} hasMore={} children={fetchData} loader={undefined} dataLength={undefined}>
-                <ul> 
-                    {props.map(prop => (
-                        <li key={prop.artwork.id}>
-                            <ListArtwork artwork={prop.artwork} />
-                        </li>
+            {/* <InfiniteScroll next={fetchData} hasMore={} children={fetchData} loader={undefined} dataLength={undefined}> */}
+             <ul> 
+                {props.artworks.map(artwork => (
+                    <li key={artwork.id}>
+                        <ListArtwork artwork={artwork} />
+                    </li>
                 ))}
-                </ul>
-            </InfiniteScroll>
+            </ul>
+            {/* </InfiniteScroll> */}
         </div>
     )
 }
