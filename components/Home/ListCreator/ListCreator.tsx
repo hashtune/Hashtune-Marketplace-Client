2b72ae504b4a5a177a2788dd9828f71f12bdbe20
Alex <alex.alber.aa@gmail.com>
2021-09-30 00:43:59 +0200

    feat: Changed List Creator to new requirements

--- components/Home/ListCreator/ListCreator.tsx
+++ components/Home/ListCreator/ListCreator.tsx
@@ -1,35 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image"
-import FollowButton from "./FollowButton";
 import CreatorImageHandle from "./CreatorIconHandle";
+import {ListCreatorFields} from '../../../lib/interfaces/CreatorInterfaces'
 
-export interface Creator {
-  name: string,
-  id:string,
-  handle: string
-  image: string,
-  bio: string,
-}
-export interface CreatorList {
-    creators: Creator[],
-}
-
-
-const ListCreator = (props: Creator) => {
+const ListCreator = (props: ListCreatorFields) => {
     return (
         <div>
-            <div>
-                <div>
-                    <Image src={props.image}/>
-                    <div>
-                        <button>{props.name}</button>
-                        <CreatorImageHandle handle={props.handle} image= {props.image}/>
-                        <h3>{props.bio}</h3>
-                    </div>
-                </div>
-                <FollowButton/>
-            </div>
+            <Image src={props.image} width= {346} height = {346}/>
+            <h2>{props.fullName}</h2>
         </div>
     )
 }
