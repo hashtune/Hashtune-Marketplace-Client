95a21e89bf920e09d179e26912243ac67db1f9e1
Alex <alex.alber.aa@gmail.com>
2021-09-30 00:45:14 +0200

    fix: Changed logic for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,75 +4,38 @@ import utilStyles from '../styles/utils.module.css'
 import { GetStaticProps } from 'next'
 import client from '../apollo-client'
 import { gql } from "@apollo/client";
-import { NextApiRequest, NextApiResponse } from 'next'
 import React from 'react'
 import Hero from '../components/Home/Hero/Hero'
-import {Artwork} from "../components/Home/ListArtwork/ListArtwork"
 import { useRouter } from 'next/dist/client/router'
 import ArtworkContainer from '../components/Home/ListContainer/ArtworkContainer'
 import CreatorContainer from '../components/Home/ListContainer/CreatorContainer'
-import { Creator } from '../components/Home/ListCreator/ListCreator'
-
+import { ListCreatorFields, ListCreatorFieldsProp } from '../lib/interfaces/CreatorInterfaces'
+import { ArtworkFields, ListArtworksFieldsProp } from '../lib/interfaces/ArtworkInterfaces'
+import { queryListArtworks } from '../lib/apiQueries/ArtworkQueries'
 
 export const getStaticProps:  GetStaticProps = async() => {
   const {data} = await client.query({
-    query: gql`
-    query ExampleQuery {
-      listArtworks{
-        id
-        title
-        saleType
-        image
-        description
-        listed
-        reservePrice
-        price
-        Auctions {
-          bids {
-            id
-          }
-          currentHigh
-        }
-        creator {
-          handle
-          image
-          fullName
-        }
-        latestAuction {
-          bids {
-            id
-          }
-        }
-      }
-      listCreators {
-        fullName
-        image
-        handle
-        id
-        bio
-      }
-    }
-  `,
+    query: queryListArtworks,
   });
     return {
       props: {
         allArtworks: data.listArtworks.slice(0, 10),
-        allCreators: data.listCreators.slice(0,20),
+        // allCreators: data.listCreators.slice(0,20),
         fallback: true
       },
    };
 }
 
 export default function Home ({allArtworks, allCreators}: {
-  allArtworks: Artwork[],
-  allCreators: Creator[],
+  allArtworks: ArtworkFields[],
+  allCreators: ListCreatorFields[],
 }) {
   const { query } = useRouter();
   const isArtistsTabSelected = !!query.artistsTab;
   const isAllHashtunesSelected = !!query.allHashtunes;
   // const isAuctionsSelected = !!query.auctions; STILL NEEDS TOBE IMPLEMENTED
   // const isBuyNowSeleected = !!query.buyNow;
-  if (!isArtistsTabSelected && isAllHashtunesSelected){
+  if (!isArtistsTabSelected){
     return (
       <Layout home>
         <Hero artwork = {allArtworks[0]}/>
