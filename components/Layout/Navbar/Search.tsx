8be91cb16daef95bf4fc4691dc1b79267484d518
Alex <alex.alber.aa@gmail.com>
2021-10-01 14:35:09 +0200

    fix: Changes previously made that need to be recommitted

--- components/Layout/Navbar/Search.tsx
+++ components/Layout/Navbar/Search.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import styles from './Navbar.module.scss'
 
 const Search = () => {
   return (
-    <span>
-      {/* <Image src= '/' /> INSERT SEARCH ICON HERE */}
-      {/* <form>Search</form> */}
-    </span>
+    <div className={styles["navbar__search"]} >
+        <svg className={styles["navbar__search--icon"]}>
+          <use xlinkHref="dist/icons/sprite.svg#hashtune-search"></use>
+        </svg>
+      <input type="text" className={styles["navbar__search--input"]} placeholder="Search Hashtunes"/>
+    </div>
   );
 };
 export default Search;
