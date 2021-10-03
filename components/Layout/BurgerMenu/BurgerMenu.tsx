ffd1506ba87d35fdf841a58808019233a728b261
Alex <alex.alber.aa@gmail.com>
2021-10-03 17:29:21 +0200

    feat: Added new BurgerMenu Feature

--- components/Layout/BurgerMenu/BurgerMenu.tsx
+++ components/Layout/BurgerMenu/BurgerMenu.tsx
@@ -1,28 +1,65 @@
 
-import Link from 'next/link'
-import styles from './Navbar.module.scss'
-import Image from 'next/image'
-import React, { useState } from 'react'
-import {slide as Menu} from 'react-burger-menu'
+import styles from './Menu.module.scss'
+import React, { useEffect, useState } from 'react'
 import {LinkIcon} from './LinkIcon'
+import { MenuItem } from './MenuItem'
 
 export const BurgerMenu = () => {
-    const [menuState, setMenuState] = useState(false);
-    const handleStateChange = () => setMenuState(true);
-    const closeMenu = () => setMenuState(false);
+    const [isActive, setIsActive] = useState(false);
+    let els: any;
+        
+    useEffect (() => {
+        els = {
+        	navIcon: document.getElementById('js-nav-button'),
+        	nav: document.getElementById('js-nav'),
+        	overlay: document.getElementById('js-overlay'),
+        	burgerLine: document.querySelector('.burger__line')
+        };
+    });
 
-  return( 
-      <Menu noOverlay width = {280}
-        isOpen = {menuState} onStateChange= {handleStateChange}>
-          <Link href = "/faq"><a onClick= {() => closeMenu()}>FAQ</a></Link>
-          <Link href = "/about"><a onClick= {() => closeMenu()}>About</a></Link>
-          <Link href = "/help"><a onClick= {() => closeMenu()}>Help</a></Link>
-          <Link href = "/tos"><a onClick= {() => closeMenu()}>Terms of Service</a></Link>
-          <Link href = "/privacy"><a onClick= {() => closeMenu()}>Privacy Policy</a></Link>
-          <Link href = "/communityGuidelines"><a onClick= {() => closeMenu()}>Community Guidelines</a></Link>
-          <LinkIcon icon={'instagram'} href={''}/>
-          <LinkIcon icon={'twitter'} href={''}/>
-          <LinkIcon icon={'youtube'} href={''}/>
-      </Menu>
-  )
+    const toggleMenu = () => {
+            els.burgerLine?.classList.toggle(styles['burger__line--active']);
+        	document.body.classList.toggle(styles['overflow']);
+        	els.overlay?.classList.toggle(styles['site-overlay--visible']);
+        	els.nav?.classList.toggle(styles['nav--visible']);
+            console.log("ïts getting here");
+        };
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+        toggleMenu();
+    };   
+
+    
+    //className={styles["navbar__menu-item"]} 
+    return (
+        <div >
+        <button className={styles["burger"]} aria-label="Toggle navigation" id="js-nav-button" onClick={handleClick} >
+            <svg className={styles["burger__line"]} fill="#fff" height="10" width="30">
+                <use xlinkHref="dist/icons/sprite.svg#hashtune-menu"></use>
+            </svg>
+        </button>
+        
+
+        <nav className={styles["nav"]} id="js-nav">
+        	<ul className={styles["nav__menu nav__menu--main menu menu--main"]}>
+                <MenuItem text={'FAQ'}/>
+                <MenuItem text={'About'}/>
+                <MenuItem text={'Help'}/>
+                <MenuItem text={'Contact'}/>
+                <MenuItem text={'Terms of Service'}/>
+                <MenuItem text={'Privacy Policy'}/>
+                <MenuItem text={'Community Guidelines'}/>
+        	</ul>
+            <button><h2>Become an Artist</h2></button>
+            <LinkIcon icon={'instagram'} href={''}/>
+            <LinkIcon icon={'twitter'} href={''}/>
+            <LinkIcon icon={'youtube'} href={''}/>
+            <a>Hashtune 2021 Ⓒ. All Rights Reserved.</a>
+        	
+        </nav>
+    <div className={styles["site-overlay"]} aria-hidden="true" id="js-overlay"></div>
+    </div>
+    )
+    
 }
