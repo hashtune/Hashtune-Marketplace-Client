97c53c550f8ba242802a4fc1ea2433a88c1480c3
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-10-03 14:39:14 +0200

    refactor: Hero componenets #24

--- components/Home/Hero/Countdown.tsx
+++ components/Home/Hero/Countdown.tsx
@@ -1,65 +1,69 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
+const moment = require("moment");
 
 interface CountdownProps {
-    liveAt?: Date,
+  liveAt?: Date;
 }
-const timeFormat = "DD MM YYYY hh:mm:ss"
-
-
-const moment = require('moment')
+const timeFormat: string = "DD MM YYYY hh:mm:ss";
 
 const Countdown = (props: CountdownProps) => {
-    let ending = props.liveAt && new Date(props.liveAt.getHours() + 24)
-    let difference =  props.liveAt && ending && +ending - +props.liveAt;
+  let ending = props.liveAt && new Date(props.liveAt.getHours() + 24);
+  let difference = props.liveAt && ending && +ending - +props.liveAt;
 
-    const [hours, setHours] = useState(24);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
-    const [intervalState, setIntervalState] = useState(setInterval(()=>{}));
-    const update = () => {
-        // const { timeTillDate, timeFormat } = props;
-        const then= moment(difference, timeFormat, true);
-        const now = moment();
-        const countdown = moment(then - now);
-        const hours = countdown.format('HH');
-        const minutes = countdown.format('mm');
-        const seconds = countdown.format('ss');
-        
-        setHours(hours)
-        setMinutes(minutes)
-        setSeconds(seconds)
-    }
+  const [hours, setHours] = useState(24);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [intervalState, setIntervalState] = useState(setInterval(() => {}));
+  const update = () => {
+    // const { timeTillDate, timeFormat } = props;
+    const then = moment(difference, timeFormat, true);
+    const now = moment();
+    const countdown = moment(then - now);
+    const hours = countdown.format("HH");
+    const minutes = countdown.format("mm");
+    const seconds = countdown.format("ss");
 
-    useEffect(()=>{
-        setIntervalState( setInterval(() => {
-            update();
-        }, 1000)
-    )}, [])
+    setHours(hours);
+    setMinutes(minutes);
+    setSeconds(seconds);
+  };
 
-    useEffect(()=> {
-        return()=>{
-            clearInterval()}
-    }, [])
-    
-    return (
-        <div data-cy= "cont-main-countdown">
-            <h1>Auction ending in</h1>
-            <div data-cy="cont-countdown">
-                <div data-cy="countdown-hours">
-                    {hours}
-                    <span>hours</span>
-                </div>
-                <div data-cy="countdown-minutes">
-                    {minutes}
-                    <span>minutes</span>
-                </div>
-                <div data-cy="countdown-seconds">
-                    {seconds}
-                <span>seconds</span>
-            </div>
-        </div>
-    </div>
+  useEffect(() => {
+    setIntervalState(
+      setInterval(() => {
+        update();
+      }, 1000)
     );
-}
+  }, []);
 
-export default Countdown
+  useEffect(() => {
+    return () => {
+      clearInterval();
+    };
+  }, []);
+
+  return (
+    <div className="countdown_card">
+      <h3 className="hero_card_title">Auction ending in</h3>
+      <div className="countdown_card-data">
+        <div className="countdown_card-data--item">
+          <h3 className="countdown_card-data--item-title">6 </h3>
+          {/* className="h3>6 </h-title" {hours} */}
+          <span>hours</span>
+        </div>
+        <div className="countdown_card-data--item">
+          <h3 className="countdown_card-data--item-title">29</h3>{" "}
+          {/* {minutes} */}
+          <span>minutes</span>
+        </div>
+        <div className="countdown_card-data--item">
+          <h3 className="countdown_card-data--item-title">24</h3>{" "}
+          {/* {seconds} */}
+          <span>seconds</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
