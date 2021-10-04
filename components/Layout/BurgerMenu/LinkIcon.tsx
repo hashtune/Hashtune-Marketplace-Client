cb1b8c4451bc7893ff34810734ac57bbb7b063a8
Alex <alex.alber.aa@gmail.com>
2021-10-05 01:11:07 +0200

    feat: Modified LinkIcon component

--- components/Layout/BurgerMenu/LinkIcon.tsx
+++ components/Layout/BurgerMenu/LinkIcon.tsx
@@ -2,18 +2,21 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState } from 'react'
+import styles from "./Menu.module.scss"
+
 interface IconLinkProps {
   icon: string;
   href:string;
 }
 export const LinkIcon = (props: IconLinkProps) => {
   const hrefStart = 'dist/icons/sprite.svg#hashtune-';
-  const stylesStart = 'link__icon'
   return( 
     <Link href = {props.href} >
-      <svg width= {32.5} height= {32.5}>
-        <use xlinkHref={hrefStart + props.icon}></use>
-      </svg>
+      <div className = {styles["nav__socials--linkIcon"]}>
+        <svg width= {32.5} height= {32.5}>
+          <use xlinkHref={hrefStart + props.icon}></use>
+        </svg>
+      </div>
     </Link>
   )
 }
