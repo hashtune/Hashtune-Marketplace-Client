fc36115da61e040e4bd0b62f726079f50ee23362
Alex <alex.alber.aa@gmail.com>
2021-10-05 01:11:55 +0200

    feat: Added identifier

--- components/Layout/BurgerMenu/MenuItem.tsx
+++ components/Layout/BurgerMenu/MenuItem.tsx
@@ -7,6 +7,8 @@ interface MenuItemProps {
 }
 export const MenuItem = (props: MenuItemProps) => {
   return( 
+    <div className = {styles["menu__item--wrapper"]}>
     <li className={styles["menu__item menu__item--main"]}><a className={styles["menu__link menu__link--main"]} href={props.href}>{props.text}</a></li>
+    </div>
   )
 }
