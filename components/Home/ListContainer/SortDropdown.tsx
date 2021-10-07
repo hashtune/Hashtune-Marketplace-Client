9975d4e1fa249b1f56fee5928fb5691c11a01ac8
Alex <alex.alber.aa@gmail.com>
2021-10-07 10:58:34 +0200

    refactor: Added some styles

--- components/Home/ListContainer/SortDropdown.tsx
+++ components/Home/ListContainer/SortDropdown.tsx
@@ -1,12 +1,16 @@
-
 import React from "react";
+import styles from "./Tab.module.scss";
 
 const SortDropDown = () => {
-    return (
-        <select>
-            <option value="latest">Latest</option>
-            <option value="earliest">Earliest</option>
-        </select>
-    )
-}
-export default SortDropDown
+  return (
+    <select className={styles["tab-nav__sort-dropdown"]} id={"dropdown"}>
+      <option className={styles["tab-nav__sort-option"]} value="latest">
+        Latest
+      </option>
+      <option className={styles["tab-nav__sort-option"]} value="earliest">
+        Earliest
+      </option>
+    </select>
+  );
+};
+export default SortDropDown;
