ec8d81a4976f38cea8e6cbd8bf45766aa40558d5
Alex <alex.alber.aa@gmail.com>
2021-10-13 14:16:22 +0200

    feat: Added ConnectWallet component

--- components/Layout/Navbar/ConnectWallet.tsx
+++ components/Layout/Navbar/ConnectWallet.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MetamaskContext } from "../../../hooks/connectWallet";
 import styles from "./ConnectWallet.module.scss";
+import { IconButton } from "./IconButton";
 
 export type ConnectWalletProps = {
-  closeModal: (data: boolean) => {};
+  toggleModal: any;
+  isActive: boolean;
 };
-export default function ConnectWallet<ConnectWalletProps>({ closeModal }: any) {
+
+const ConnectWallet = (props: ConnectWalletProps) => {
   // If metamask is installed then window.ethereum is available.
   // Also need to listen for account changes.
   const {
@@ -19,67 +22,91 @@ export default function ConnectWallet<ConnectWalletProps>({ closeModal }: any) {
     getChainId,
     changeTokenPrice,
   } = React.useContext(MetamaskContext);
+  useEffect(() => {
+    showModal();
+  }, [close]);
+  const overlay: React.RefObject<HTMLDivElement> = useRef(null);
+  const showModal = () => {
+    overlay.current?.classList.toggle("site-overlay--visible");
+  };
+  const handleClick = () => {
+    showModal();
+    props.toggleModal(!props.isActive);
+  };
+  // const div : HTMLDivElement = <div></div>
 
   return (
-    <div className={styles.connectWallet}>
-      <div className={styles.card}>
-        <div className={styles.header}>
-          <p onClick={() => closeModal(false)} className={styles.left}>
-            x
-          </p>
-          <h1 className={styles.title}>Connect Wallet</h1>
-          <div className={styles.right}></div>
-        </div>
-
-        <div className={styles.cardBody}>
-          <p className={styles.policy}>
-            By connecting your wallet, you agree to our Terms of Service and
-            Privacy Policy
-          </p>
-          {account ? (
-            <>{account}</>
-          ) : (
-            <div className={styles.buttons}>
-              <button
-                id="connect1"
-                className={styles.one}
-                disabled={walletConnected}
-                onClick={() => {
-                  getAccount();
-                  getNetwork();
-                  getChainId();
-                }}
-              >
-                {walletConnected ? "Wallet Connected" : "Metamask"}
-              </button>
-              <button
-                id="connect2"
-                disabled={true}
-                className={styles.two}
-                onClick={() => {
-                  getAccount();
-                  getNetwork();
-                  getChainId();
-                }}
-              >
-                {walletConnected ? "Wallet Connected" : "WalletConnect"}
-              </button>
+    <div>
+      <div className={styles["connectWallet"]}>
+        <div className={styles["card"]}>
+          <div className={styles["header"]}>
+            <div
+              className={styles["header__close"]}
+              onClick={() => handleClick()}
+            >
+              <div className={styles["header__close--up"]}></div>
+              <div className={styles["header__close--down"]}></div>
             </div>
-          )}
-          <div className={styles.wrongNetwork}>
-            {networkConnected
-              ? ""
-              : "You need to connect to the binance test network"}
+            <a className={styles["title"]}>Connect Wallet</a>
+            <div className={styles["right"]}></div>
           </div>
-          {/* <div>active account: {account}</div>
+
+          <div className={styles["cardBody"]}>
+            <p className={styles["policy"]}>
+              By connecting your wallet, you agree to our{" "}
+              <b>Terms of Service</b> and <b>Privacy Policy</b>
+            </p>
+            <div className={styles["buttons__wrapper"]}>
+              {account ? (
+                <>{account}</>
+              ) : (
+                <div className={styles["buttons"]}>
+                  <IconButton
+                    icon={"metamask"}
+                    title={"Metamask"}
+                    onClick={() => {
+                      getAccount();
+                      getNetwork();
+                      getChainId();
+                    }}
+                    walletConnected={walletConnected}
+                    class={"buttons__btn--one"}
+                  />
+                  <IconButton
+                    icon={"wallet-connect"}
+                    title={"WalletConnect"}
+                    onClick={() => {
+                      getAccount();
+                      getNetwork();
+                      getChainId();
+                    }}
+                    walletConnected={walletConnected}
+                    class={"buttons__btn--two"}
+                  />
+                </div>
+              )}
+              <p className={styles["wrongNetwork"]}>
+                {networkConnected
+                  ? ""
+                  : "You need to connect to the binance test network"}
+              </p>
+            </div>
+            {/* <div>active account: {account}</div>
           <div>active network: {network}</div>
           <div>active chain Id: {chainId}</div> */}
-          <p> Don't have a wallet?</p>
-          <p>Check our complete guide on wallets</p>
+            <div className={styles["footer"]}>
+              <p>
+                <b>Don't have a wallet?</b>
+              </p>
+              <p>Check our complete guide on wallets.</p>
+            </div>
+          </div>
         </div>
+        {/* <button onClick={changeTokenPrice}>change price of existing token</button> */}
       </div>
-
-      {/* <button onClick={changeTokenPrice}>change price of existing token</button> */}
+      <div ref={overlay} className={"site-overlay"}></div>
     </div>
   );
-}
+};
+
+export default ConnectWallet;
