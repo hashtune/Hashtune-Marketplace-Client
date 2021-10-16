8de69f645262f40a8c5a19474364ed2f946c8cbe
Alex <alex.alber.aa@gmail.com>
2021-10-17 00:21:56 +0200

    feat: Moved ArtworkTitleCreator inside ListArtwork

--- components/Home/ListArtwork/ListArtwork.tsx
+++ components/Home/ListArtwork/ListArtwork.tsx
@@ -11,21 +11,22 @@ const ListArtwork = (props: ListArtworkFieldsProp) => {
   const artwork = props.artwork;
   const coverImage = `/dist/images/mock/artworks/${randomMockMedia(16)}.png`;
   return (
-    <div>
-      <div className="artworks__artwork_data">
-        <div className={styles["artworks__artwork_data--image-container"]}>
-          <Image
-            alt="list cover image"
-            src={coverImage}
-            width={368}
-            height={368}
-            className={styles["artworks__artwork_data--image"]}
-          />
+    <div className="artworks__artwork_data">
+      <div className={styles["artworks__artwork_data--image-container"]}>
+        <Image
+          alt="list cover image"
+          src={coverImage}
+          width={368}
+          height={368}
+          className={styles["artworks__artwork_data--image"]}
+        />
+      </div>
+      <div className={styles["artworks__artwork_data--content"]}>
+        <div className={styles["artwork__title-creator"]}>
+          <h3>{artwork.title}</h3>
+          <p>{artwork.creator.fullName}</p>
         </div>
-        <div className={styles["artworks__artwork_data--content"]}>
-          <ArtworkTitleCreator artwork={artwork} />
-          <Price artwork={artwork} />
-        </div>
+        <Price artwork={artwork} />
       </div>
     </div>
   );
