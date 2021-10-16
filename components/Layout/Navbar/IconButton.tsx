7233439e7ec97bd7bae9c3b2d74721304b1fa242
Alex <alex.alber.aa@gmail.com>
2021-10-16 15:50:27 +0200

    feat: Added transformations to the IconButtons for when connecting to wallet

--- components/Layout/Navbar/IconButton.tsx
+++ components/Layout/Navbar/IconButton.tsx
@@ -1,37 +1,61 @@
-import Link from "next/link";
-import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import styles from "./ConnectWallet.module.scss";
+import React from "react";
 
 interface IconButtonProps {
-  icon: string;
   title: string;
   onClick: () => void;
   walletConnected: boolean;
+
+  isAuthenticating: boolean;
   class: string;
+  firstLoad: boolean;
 }
+
 export const IconButton = (props: IconButtonProps) => {
   const hrefStart = "dist/icons/sprite.svg#hashtune-";
-  const metamaskBtn: React.RefObject<HTMLAnchorElement> = useRef(null);
+  const btn: React.RefObject<HTMLAnchorElement> = useRef(null);
 
-  const [isAuthenticatingMetaMask, setAuthenticatingMetaMask] = useState(false);
+  const toggleAuthenticating = () => {
+    btn.current?.classList.toggle(styles["buttons__btn--one--active"]);
+  };
+
+  const isMetamask = props.title == "Metamask";
+  const icon = isMetamask ? "metamask" : "wallet-connect";
+
+  const [styleClass, setStyleClass] = useState(props.class);
+
   useEffect(() => {
-    metamaskBtn.current?.classList.toggle(styles["buttons__btn--one--active"]);
-  }, [isAuthenticatingMetaMask]);
+    //TODO still needs to start off Metamask at 100% size and toggle to 80% instead of other way around
+    if (styleClass === styles["buttons__btn--one"]) {
+      toggleAuthenticating();
+    }
+  }, [styleClass]);
 
   return (
     <a
       onClick={() => {
-        setAuthenticatingMetaMask(true);
-        props.onClick;
+        props.onClick();
+        {
+          props.isAuthenticating
+            ? setStyleClass(styles["buttons__btn--one"])
+            : setStyleClass(props.class);
+        }
       }}
-      className={styles[props.class]}
-      ref={metamaskBtn}
+      className={styleClass}
+      ref={btn}
     >
       <svg width={30} height={30}>
-        <use xlinkHref={hrefStart + props.icon}></use>
+        <use xlinkHref={hrefStart + icon}></use>
       </svg>
       <h2>{props.walletConnected ? "Wallet Connected" : props.title}</h2>
     </a>
   );
 };
+
+// USEFUL PRINT:
+// console.log(
+//       isMetamask
+//         ? "MetamaskBtn classList" + btn.current?.classList
+//         : "WalletConnect classList" + btn.current?.classList
+//     );
