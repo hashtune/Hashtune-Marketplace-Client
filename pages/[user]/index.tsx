8ca9773646b8d66d216f953bea78072624cebd00
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-10-16 12:37:40 +0200

    feat: Profile content initial skeleton structure #72

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -1,12 +1,14 @@
 import { gql } from "@apollo/client";
 import { GetServerSidePropsContext } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./User.module.scss";
 
 import client from "../../lib/apollo-client";
 import React from "react";
 import { Navbar } from "../../components/Layout/Navbar/Navbar";
 import { randomMockMedia } from "../../utils/index";
+import { CreatorFields } from "../../lib/interfaces/CreatorInterfaces";
 
 // TODO: Refactor page/query
 
@@ -26,6 +28,24 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
             wallet {
               provider
               publicKey
+            }
+            created {
+              kind
+              handle
+              title
+              id
+              image
+              description
+              listed
+              price
+              reservePrice
+              saleType
+              Auctions {
+                id
+                currentHigh
+                liveAt
+                artworkId
+              }
             }
           }
         }
@@ -53,7 +73,9 @@ const profilePicture = `/dist/images/mock/users/${randomMockMedia(12)}.png`;
 // const profilePicture = `/dist/images/mock/users/15.png`;
 const coverImage = `/dist/cover.png`;
 
+const artworkImage = `/dist/images/mock/artworks/${randomMockMedia(20)}.png`;
 export default function User(singleUser: any) {
+  console.log(singleUser.singleUser.created);
   return (
     <div>
       <Navbar />
@@ -146,10 +168,92 @@ export default function User(singleUser: any) {
               </div>
             </aside>
             <div className={styles["user-profile-content__artworks"]}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-              necessitatibus excepturi quae modi saepe, a veniam iste sed nihil
-              beatae numquam sit aperiam? Dolor vero quas adipisci ducimus id
-              optio!
+              <div className={styles["artworks"] + " container"}>
+                <div className={"tab-nav" + " " + "tab-nav__container"}>
+                  <div className="tab-nav__indicators">
+                    <a
+                      className="tab-nav__indicators--element"
+                      // onClick={() => setTabState("All Hashtunes")}
+                    >
+                      Created
+                    </a>
+                    <a
+                      className="tab-nav__indicators--element"
+                      // onClick={() => setTabState("Auctions")}
+                    >
+                      Collected
+                    </a>
+                    <a
+                      className="tab-nav__indicators--element"
+                      // onClick={() => setTabState("Buy Now")}
+                    ></a>
+                  </div>
+                  <div className="tab-nav__dropdown">
+                    {/* <SortDropDown /> */}
+                  </div>
+                </div>
+
+                <div className={styles["artworks__container"]}>
+                  {singleUser.singleUser.created.length > 0 &&
+                    singleUser.singleUser.created?.map(
+                      (userArtworks: CreatorFields) => (
+                        <div
+                          key={userArtworks.id}
+                          className={styles["artworks__artwork"]}
+                        >
+                          <Link
+                            href={`/${userArtworks.handle}/${userArtworks.id}`}
+                          >
+                            <a>
+                              <div className="artworks__artwork_data">
+                                <div
+                                  className={
+                                    styles[
+                                      "artworks__artwork_data--image-container"
+                                    ]
+                                  }
+                                >
+                                  <Image
+                                    alt="list cover image"
+                                    src={artworkImage}
+                                    width={280}
+                                    height={280}
+                                    className={
+                                      styles["artworks__artwork_data--image"]
+                                    }
+                                  />
+                                </div>
+                                <div
+                                  className={
+                                    styles["artworks__artwork_data--content"]
+                                  }
+                                >
+                                  <div
+                                    className={
+                                      styles["artworks__artwork_data--content"]
+                                    }
+                                  >
+                                    <div data-cy="artwork-title-creator">
+                                      <h3 data-cy="artwork-title">
+                                        {userArtworks.title}
+                                      </h3>
+                                      <p data-cy="artwork-creator-fullName">
+                                        {userArtworks.fullName}
+                                      </p>
+                                    </div>
+                                    <div className="sales-type-badge">
+                                      <h3>Make an offer</h3>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </a>
+                          </Link>
+                        </div>
+                      )
+                    )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
