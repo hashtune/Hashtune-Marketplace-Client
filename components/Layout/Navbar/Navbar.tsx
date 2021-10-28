e1fb5f74e7658a7e7ed62f03bd3324019c72a709
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-10-28 20:09:30 +0200

    feat: Adding navbar view when visiting register route

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -7,12 +7,13 @@ import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 import { MetamaskContext } from "../../../hooks/connectWallet";
 import React, { useEffect, useRef } from "react";
 import ConnectWallet from "./ConnectWallet";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
   const [connectVisible, setConnectVisible] = React.useState(false);
 
   const overlay: React.RefObject<HTMLDivElement> = useRef(null);
-
+  const router = useRouter();
   const {
     account,
     walletConnected,
@@ -23,6 +24,7 @@ export const Navbar = () => {
   } = React.useContext(MetamaskContext);
 
   const walletState = () => {
+
     if (fetchingAccount || fetchingChain || fetchingNetwork) {
       return <></>;
     } else if (!walletConnected) {
@@ -44,32 +46,54 @@ export const Navbar = () => {
     }
   };
 
-  return (
-    <nav className={styles["navbar"]} data-cy="navbar">
-      <div className={styles["navbar__logo"]} data-cy="navbar-logo">
-        <Link href="/">
-          <a>
-            <Image src="/images/logo.svg" width={133} height={32} />
-          </a>
-        </Link>
-      </div>
-      <div className={styles["navbar__menu"]} data-cy="navbar-menu">
-        <div className={styles["navbar__menu-item"]} data-cy="navbar-menu-item">
-          <Tab href="/" title="Songs" icon="record" />
+  if(router.pathname !== "/register"){
+    return (
+      <nav className={styles["navbar"]} data-cy="navbar">
+        <div className={styles["navbar__logo"]} data-cy="navbar-logo">
+          <Link href="/">
+            <a>
+              <Image src="/images/logo.svg" width={133} height={32} />
+            </a>
+          </Link>
         </div>
-        <div className={styles["navbar__menu-item"]} data-cy="navbar-menu-item">
-          <Tab href="/artists" title="Artists" icon="music-note" />
+        <div className={styles["navbar__menu"]} data-cy="navbar-menu">
+          <div className={styles["navbar__menu-item"]} data-cy="navbar-menu-item">
+            <Tab href="/" title="Songs" icon="record" />
+          </div>
+          <div className={styles["navbar__menu-item"]} data-cy="navbar-menu-item">
+            <Tab href="/artists" title="Artists" icon="music-note" />
+          </div>
         </div>
-      </div>
-      <Search />
-      {walletState()}
-      <BurgerMenu />
-      {connectVisible === true && (
-        <ConnectWallet
-          isActive={connectVisible}
-          toggleModal={setConnectVisible}
-        ></ConnectWallet>
-      )}
-    </nav>
-  );
+        <Search />
+        {walletState()}
+        <BurgerMenu />
+        {connectVisible === true && (
+          <ConnectWallet
+            isActive={connectVisible}
+            toggleModal={setConnectVisible}
+          ></ConnectWallet>
+        )}
+      </nav>
+    );
+  } else {
+    return (
+      <nav className={styles["navbar"] + " flex-content-center"} data-cy="navbar">
+        <div className={styles["navbar__logo"]} data-cy="navbar-logo">
+          <Link href="/">
+            <a>
+              <Image src="/images/logo.svg" width={133} height={32} />
+            </a>
+          </Link>
+        </div>
+        {/* {walletState()}
+        <BurgerMenu />
+        {connectVisible === true && (
+          <ConnectWallet
+            isActive={connectVisible}
+            toggleModal={setConnectVisible}
+          ></ConnectWallet>
+        )} */}
+      </nav>
+    )
+  }
 };
