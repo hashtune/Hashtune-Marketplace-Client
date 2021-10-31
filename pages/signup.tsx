41fd631ee8265d40ca6ce9c3d662e8bfb110884f
eemmiillyy <emily.morgan@code.berlin>
2021-10-31 10:19:42 +0100

    Hook up mutation

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,21 +1,30 @@
-import { GetStaticProps } from "next";
-import client from "../lib/apollo-client";
 import React from "react";
-import { useRouter } from "next/dist/client/router";
 import { Navbar } from "../components/Layout/Navbar/Navbar";
 import styles from "../styles/pages/Signup.module.scss";
 import { Formik, Field } from "formik";
 import { InputField } from "../components/Fields/InputField";
-import { ApolloProvider, useMutation } from "@apollo/client";
-import { registerUserMutation } from "../graphql/user/mutations/register";
-
-import {
-  RegisterUserDocument,
-  useRegisterUserMutation,
-} from "../graphql/generated/apolloComponents";
+import { useRegisterUserMutation } from "../graphql/generated/apolloComponents";
+import { MetamaskContext } from "../hooks/connectWallet";
 
 export default function Signup() {
-  // const [registerUser] = useMutation(registerUserMutation);
+  const [
+    registerUserMutation,
+    { data, loading, error: registerError },
+  ] = useRegisterUserMutation();
+
+  const { account } = React.useContext(MetamaskContext);
+  // TODO: If no account connected - figure out if we want to redierct
+  // prompt the user etc...
+  if (!account) return null;
+
+  const validate = (value: any) => {
+    let errorMessage;
+    // TODO: Fix this regex to support code.berlin emails
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      errorMessage = "Invalid email address";
+    }
+    return errorMessage;
+  };
 
   return (
     <div className={"app " + styles["register__layout"]}>
@@ -27,23 +36,40 @@ export default function Signup() {
         </div>
         <Formik
           onSubmit={async (_data) => {
-            console.log(_data);
+            await registerUserMutation({
+              variables: {
+                inputType: {
+                  email: _data.email,
+                  wallet: account,
+                  handle: _data.handle,
+                  bio: "",
+                  fullName: _data.fullName,
+                  image: "",
+                  isApprovedCreator: false,
+                },
+              },
+            });
+            // TODO on success redirect to profile page
+            // TODO make sure the back end validates that the required fields are NOT empty strings
+            // TODO make sure the back end returns a proper error instead of throwing what it currently does
+            // when the unique fields are invalid
           }}
           initialValues={{
             email: "",
             fullName: "",
             handle: "",
-            wallet: "",
+            wallet: account,
+            bio: "",
           }}
         >
-          {({ handleSubmit }) => (
+          {({ handleSubmit, errors, touched }) => (
             <form className={styles["register__form"]} onSubmit={handleSubmit}>
               <div className="input__group">
                 <label htmlFor="">Wallet</label>
                 <Field
                   type="text"
                   className="text_input"
-                  value="0x0adb6821bae3c60d2262753e90a0953ff60d22627"
+                  value={account}
                   disabled
                   component={InputField}
                 />
@@ -79,7 +105,11 @@ export default function Signup() {
                   className="text_input"
                   placeholder="E.g humam@hashtune.co"
                   component={InputField}
+                  validate={validate}
                 />
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
               </div>
               <hr className={styles["register__divider"]} />
 
@@ -97,7 +127,7 @@ export default function Signup() {
                   <a href="">Terms of use</a>
                 </label>
               </div>
-
+              {registerError && <p>{registerError.message}</p>}
               <button
                 type="submit"
                 className={styles["primary_button"] + " primary_button"}
