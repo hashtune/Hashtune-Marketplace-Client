c66385951f7c7bbe3372cb4783fa9f18918db3dd
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-11-07 05:39:30 +0100

    refactor: Upload variant Layout #91

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -10,50 +10,56 @@ export default function Upload() {
     <div className={"app " + styles["upload__layout"]}>
       <Navbar />
       <div className={styles["upload__hero"]}>
-        <div className={styles["upload__content"]}>
-          <h2>Upload your Music 🎸</h2>
-          <p>
-            Choose “Single” if you want your collectible song to be one of a
-            kind or “Album” if you want to sell one collectible multiple times
-          </p>
-        </div>
-        <div className={styles["upload__variant"]}>
-          <div className={styles["upload__variant-container"]}>
-            <div className={styles["upload__variant-type"]}>
-              <Image
-                src="/dist/patterns/upload-type-pattern.png"
-                width="240"
-                height="240"
-                alt=""
-                draggable="false"
-              />
-              <a
-                href=""
-                className={
-                  "primary_button " + styles["upload__variant-type--button"]
-                }
-              >
-                Upload Single
-              </a>
-            </div>
-            <div className={styles["upload__variant-type"]}>
-              <Image
-                src="/dist/patterns/upload-type-pattern-1.png"
-                width="240"
-                height="240"
-                alt=""
-                draggable="false"
-              />
-              <a
-                href=""
-                className={
-                  "primary_button " + styles["upload__variant-type--button"]
-                }
-              >
-                Upload Album
-              </a>
+        <div className={styles["upload__hero-container"]}>
+          <div className={styles["upload__content"]}>
+            <h2>Upload your Music 🎸</h2>
+            <p>
+              Choose “Single” if you want your collectible song to be one of a
+              kind or “Album” if you want to sell one collectible multiple times
+            </p>
+          </div>
+          <div className={styles["upload__variant"]}>
+            <div className={styles["upload__variant-container"]}>
+              <div className={styles["upload__variant-type"]}>
+                <Image
+                  src="/dist/patterns/upload-type-pattern.png"
+                  width="240"
+                  height="240"
+                  alt=""
+                  draggable="false"
+                />
+                <a
+                  href=""
+                  className={
+                    "primary_button " + styles["upload__variant-type--button"]
+                  }
+                >
+                  Upload Single
+                </a>
+              </div>
+              <div className={styles["upload__variant-type"]}>
+                <Image
+                  src="/dist/patterns/upload-type-pattern-1.png"
+                  width="240"
+                  height="240"
+                  alt=""
+                  draggable="false"
+                />
+                <a
+                  href=""
+                  className={
+                    "primary_button " + styles["upload__variant-type--button"]
+                  }
+                >
+                  Upload Album
+                </a>
+              </div>
             </div>
           </div>
+          <p className={styles["upload__rights"]}>
+            We do not own your private keys and cannot access your funds without
+            your confirmation.
+          </p>
         </div>
       </div>
     </div>
