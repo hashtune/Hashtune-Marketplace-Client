26a6a62375863e0a37324db604d00d86f676beee
Alex <alex.alber.aa@gmail.com>
2021-11-10 22:37:49 +0100

    feat: Made CountDown more reusable

--- components/Home/Hero/Countdown.tsx
+++ components/Home/Hero/Countdown.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 const moment = require("moment");
 import styles from "./Hero.module.scss";
+import { PAGES } from "../../../utils/constants/enum";
 
 interface CountdownProps {
   liveAt?: Date;
-  style: string;
+  page?: string;
 }
 const timeFormat: string = "DD MM YYYY hh:mm:ss";
 
@@ -44,25 +45,29 @@ const Countdown = (props: CountdownProps) => {
     };
   }, []);
 
+  const [style, setStyle] = useState("countdown");
+
+  useEffect(() => {
+    if (props.page === PAGES.ARTWORK) {
+      setStyle("countdown-artwork");
+    }
+  });
+
   return (
-    <div className={styles["countdown_card"] + " " + props.style}>
-      <h3 className={props.style + "-title"}>Auction ending in</h3>
-      <div
-        className={styles["countdown_card-data"] + " " + props.style + "-data"}
-      >
-        <div className={props.style + "-data--item"}>
-          <h3 className={props.style + "-data--item-title"}>6 </h3>
+    <div className={styles["countdown_card"] + " " + style}>
+      <h3 className={style + "__title"}>Auction ending in</h3>
+      <div className={styles["countdown-data"] + " " + style + "__data"}>
+        <div className={style + "__data--item"}>
+          <h3>6 </h3>
           {/* className="h3>6 </h-title" {hours} */}
           <span>hours</span>
         </div>
-        <div className={props.style + "-data--item"}>
-          <h3 className={props.style + "-data--item-title"}>29</h3>{" "}
-          {/* {minutes} */}
+        <div className={style + "__data--item"}>
+          <h3>29</h3> {/* {minutes} */}
           <span>minutes</span>
         </div>
-        <div className={props.style + "-data--item"}>
-          <h3 className={props.style + "-data--item-title"}>24</h3>{" "}
-          {/* {seconds} */}
+        <div className={style + "__data--item"}>
+          <h3>24</h3> {/* {seconds} */}
           <span>seconds</span>
         </div>
       </div>
