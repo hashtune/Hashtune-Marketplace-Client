05357f7a77ddc86d196f99c23892701a75a156e9
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-11-10 22:38:46 +0100

    feat: Fix icons #91

--- components/Layout/Navbar/Tab.tsx
+++ components/Layout/Navbar/Tab.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import React from "react";
 
 interface TabProps {
-    href: string,
-    title: string,
-    icon: string
+  href: string;
+  title: string;
+  icon: string;
 }
 const Tab = (props: TabProps) => (
-    <Link href={props.href || "/"}>
-        <a>
-            <svg fill="#fff">
-                <use xlinkHref={`dist/icons/sprite.svg#hashtune-${props.icon}`}></use>
-            </svg>
-            {props.title}
-        </a>
-    </Link>
-)
+  <Link href={props.href || "/"}>
+    <a>
+      <svg fill="#fff">
+        <use xlinkHref={`/dist/icons/sprite.svg#hashtune-${props.icon}`} />
+      </svg>
+      {props.title}
+    </a>
+  </Link>
+);
 export default Tab;
