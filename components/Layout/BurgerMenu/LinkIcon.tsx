88142404cecf061ca403558c6afba5105e0ee510
Alex <alex.alber.aa@gmail.com>
2021-11-10 22:33:27 +0100

    feat: Fixed Link Icon component

--- components/Layout/BurgerMenu/LinkIcon.tsx
+++ components/Layout/BurgerMenu/LinkIcon.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./BurgerMenu.module.scss";
+import { PAGES } from "../../../utils/constants/enum";
 
 interface IconLinkProps {
   icon: string;
   href: string;
+  // directorydifference: number;
+  pre: string;
 }
 export const LinkIcon = (props: IconLinkProps) => {
   const hrefStart = "dist/icons/sprite.svg#hashtune-";
   return (
-    <Link href={props.href}>
-      <div className={styles["nav__socials--linkIcon"]}>
-        <svg width={20} height={20}>
-          <use xlinkHref={hrefStart + props.icon}></use>
-        </svg>
-      </div>
-    </Link>
+    <a href={props.href}>
+      <svg>
+        <use xlinkHref={props.pre + hrefStart + props.icon} />
+      </svg>
+    </a>
   );
 };
