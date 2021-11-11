58ba27876315f75112fe8e16cfb355aacc66077f
Alex <alex.alber.aa@gmail.com>
2021-11-11 14:37:31 +0100

    ref: Refactored ArtworkContainer, can now be properly reused in Profile page

--- components/Home/ListContainer/ArtworkContainer.tsx
+++ components/Home/ListContainer/ArtworkContainer.tsx
@@ -4,86 +4,80 @@ import ListArtwork from "../ListArtwork/ListArtwork";
 import { ListArtworksProps } from "../../../lib/interfaces/ArtworkInterfaces";
 import Link from "next/link";
 import client from "../../../lib/apollo-client";
-import { Artwork, QueryListArtworksQuery } from "../../../graphql/generated/apolloComponents";
+import { Artwork, QueryListArtworksQuery, User } from "../../../graphql/generated/apolloComponents";
 import SortDropDown from "./SortDropdown";
 import { listArtworkQuery } from "../../../graphql/artwork/queries/listArtworks";
 import { DocumentNode } from "graphql";
 import Tab from "./Tab";
 
-export interface TabArtworksProps {
+export interface ArtworkContainerProps {
   artworks: Artwork[];
-  type: string;
+  creator?: User;
   tabs: string[];
+  page: string;
 }
 
-const ArtworkContainer = (props: TabArtworksProps) => {
+const ArtworkContainer = (props: ArtworkContainerProps) => {
   const [artworks, setArtworks] = useState(props.artworks);
-  const [tabState, setTabState] = useState("All Hashtunes");
+  const [tabState, setTabState] = useState(props.tabs[0]);
+  const created = props.creator?.created!==undefined ? props.creator?.created : [];
+  const owned = props.creator?.owned!==undefined ? props.creator?.owned : [];
 
   useEffect(() => {
-    
-    if (tabState === "Auctions") {
-      getAuctions();
-    } else if (tabState === "Buy Now") {
-      getBuyNow();
-    } else {
-      getAllHashtunes();
+     if (tabState== "Created"){
+      setArtworks(created);
+    } else if (tabState== "Collected"){
+      setArtworks(owned);
+    } else if (tabState=== "Auctions" || "Buy Now" || "All Hashtunes") {
+      updateArtworks(tabState);
     }
   }, [tabState]);
 
-  const getAuctions = async () => {
+  const updateArtworks= async(tab: string) => {
+    let listed : boolean = true;
+    let auction : boolean = false;
+    if (tab==="Auctions"){
+      auction = true;
+    } else if (tab==="Buy Now"){
+      auction = false;
+    }
     const res = await client.query({
       query: listArtworkQuery,
-      variables: { listArtworksAuction: true, listArtworksListed: true },
+      variables: tab!=="All Hashtunes" ? { auction, listed }: { }
     });
     setArtworks(res.data.listArtworks.Artworks);
-  };
-
-  const getBuyNow = async () => {
-    const res = await client.query({
-      query: listArtworkQuery,
-      variables: { listArtworksAuction: false, listArtworksListed: true },
-    });
-    setArtworks(res.data.listArtworks.Artworks);
-  };
-
-  const getAllHashtunes = async () => {
-    const res = await client.query({
-      query: listArtworkQuery,
-    });
-    setArtworks(res.data.listArtworks.Artworks);
-  };
+  }
 
 
   return (
     <div className={styles["artworks"] + " container"}>
       <div className="tab-nav tab-nav__container">
-
         <div className="tab-nav__indicators">
           {props.tabs!== undefined && props.tabs.length > 0 &&
           props.tabs?.map((tab) => (
-            <Tab onClick={() => setTabState(tab)} style={"tab-nav__indicators--element"} text={tab}/>
+            <Tab onClick={() => {
+              setTabState(tab);
+            } } text={tab} key={tab}/>
           ))}
+          
+      </div>
         <div className="tab-nav__dropdown">
           <SortDropDown />
         </div>
       </div>
-
       <div className={styles["artworks__container"]}>
-        {artworks.length > 0 &&
+        {artworks !== undefined && artworks.length > 0 &&
           artworks?.map((artwork) => (
             <div key={artwork.id} className={styles["artworks__item"]}>
               <Link href={`/${artwork.creator.handle}/${artwork.id}`}>
                 <a>
-                  <ListArtwork imageSize={368} artwork={artwork} />
+                  <ListArtwork page = {props.page} imageSize={368} artwork={artwork} />
                 </a>
               </Link>
             </div>
           ))}
       </div>
     </div>
-    
-        </div>
   );
 };
 export default ArtworkContainer;
