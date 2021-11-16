5a0a7e1e2dd2cb5a0b3991564af9c5dff8814019
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-11-16 20:50:33 +0100

    fix: Adding alt tag to images

--- pages/[user]/index.tsx
+++ pages/[user]/index.tsx
@@ -73,7 +73,7 @@ export default function User(singleUser: any) {
 				<div className="container_relative">
 					<div className={styles['user-profile-details']}>
 						<div className={styles['user-profile-details__picture']}>
-							<Image src={profilePicture} width="180" height="180" alt="" />
+							<Image src={profilePicture} width="180" height="180" alt={singleUser?.fullName} />
 						</div>
 						<div className={styles['user-profile-details__content']}>
 							<div className={styles['user-profile-details__content-primary']}>
