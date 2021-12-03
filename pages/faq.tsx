b96eb2c2ca4621229b9c30646c14c9f24f07f559
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-12-03 15:28:11 +0100

    style: Add paragraph-title utility class

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -7,7 +7,7 @@ export default function Explore() {
       <Navbar />
       <div className="static-content-container">
         <h1>FAQ</h1>
-        <p>HOW MUCH DO ARTISTS TAKE?</p>
+        <h2 className="title-paragraph">How much do artists take?</h2>
         <p>
           Artists receive close to 100% of their initial record sale, and then
           receive a percentage or "share" that they alone specify, on every
@@ -15,7 +15,7 @@ export default function Explore() {
           for other artists who contributed to the record.
         </p>
         <br />
-        <p>WILL MY TOKEN EVERY DISAPPEAR?</p>
+        <h2 className="title-paragraph">Will my token ever disappear?</h2>
         <p>
           No. We store the permanent record data on Inter Planetary File System
           (IPFS), because IPFS is distributed rather than centralised, so it
@@ -25,7 +25,7 @@ export default function Explore() {
           in terms of transactional costs or "gas".
         </p>
         <br />
-        <p>HOW IS MY TOKEN VALUABLE?</p>
+        <h2 className="title-paragraph">How is my token valuable?</h2>
         <p>
           The actual artwork is accessible for anyone to listen to at any time,
           with or without an account, the difference is that we introduce a
