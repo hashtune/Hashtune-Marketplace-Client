3c7579b051ce72c2338b159eac4fca5efc201876
Humam Abo Alraja <humamaboalraja@gmail.com>
2021-12-11 21:19:23 +0100

    feat: Single NFT live preview functionality

--- pages/upload/single.tsx
+++ pages/upload/single.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../../components/Layout/Navbar/Navbar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Field } from "formik";
 import { useRegisterUserMutation } from "../../graphql/generated/apolloComponents";
 import router from "next/router";
@@ -14,6 +14,8 @@ import gql from "graphql-tag";
 import { checkHandleFree } from "../../lib/apiQueries/ArtworkQueries";
 import { SaleType } from "../../hooks/connectWallet";
 import { Session } from "../../hooks/session";
+import { link } from "fs";
+import PlayerContainer from "../../components/Audio/PlayerContainer";
 
 export { getServerSideProps } from "../../hooks/session";
 
@@ -33,11 +35,16 @@ export type CreateInputType = {
 };
 export default function CreatePage({ session }: { session: Session }) {
   const { createNFT } = React.useContext(MetamaskContext);
-  const [handle, setHandle] = React.useState<string>("");
+  const [song, setSong] = React.useState<string>("/dist/audio/4.mp3");
+  const [handle, setHandle] = React.useState<string>("NFT-single-handle");
   const [handleFree, setHandleFree] = React.useState<boolean>(true);
-  const [title, setTitle] = React.useState<string>("default");
-  const [image, setImage] = React.useState<string>("default");
-  const [description, setDescription] = React.useState<string>("default");
+  const [title, setTitle] = React.useState<string>("NFT Single title");
+  const [image, setImage] = React.useState<string>(
+    "/dist/images/mock/artworks/1.png"
+  );
+  const [description, setDescription] = React.useState<string>(
+    "NFT Single Description"
+  );
   const [saleType, setSaleType] = React.useState<SaleType>("fixed");
   const [reservePrice, setReservePrice] = React.useState<string>("0");
   const [salePrice, setSalePrice] = React.useState<string>("0");
@@ -161,13 +168,47 @@ export default function CreatePage({ session }: { session: Session }) {
     setReservePrice(val);
   };
 
+  const songChange = (val: string) => {
+    setSong(val);
+  };
+
   const creatorsListChange = (address: string[]) => {};
   const creatorsRoyaltiesChange = (cuts: number[]) => {};
 
   // export default function Single() {
   const onFileChange = (files: any) => {
-    console.log(files);
+    // const selectedFile = files.target.input.files[0];
+    // console.log("Selected File", selectedFile);
+    // var data = new FormData();
+    // data.append("file", selectedFile);
+    // console.log("Form Data", data);
   };
+
+  const renderImage = () => {
+    if (image === "" || !image) {
+      return (
+        <Image
+          alt="cover image"
+          src="/dist/images/mock/artworks/1.png"
+          width={600}
+          height={600}
+        />
+      );
+    } else {
+      return (
+        <Image
+          alt="cover image"
+          src={image}
+          width={600}
+          height={600}
+          defaultValue="/dist/images/mock/artworks/1.png"
+        />
+      );
+    }
+  };
+
+  console.log(renderImage());
+
   return (
     <div className={"app " + styles["single__layout"]}>
       <Navbar session={session} />
@@ -178,7 +219,17 @@ export default function CreatePage({ session }: { session: Session }) {
           <h1>Upload your Music 🎸</h1>
 
           <div className={styles["register__form"] + " mt-small"}>
-            <DropFileInput onFileChange={(files) => onFileChange(files)} />
+            {/* <DropFileInput onFileChange={(files) => onFileChange(files)} /> */}
+            <div className="input__group">
+              <label htmlFor="">NFT Single Song </label>
+              <input
+                className="text_input"
+                datatype="file"
+                type="file"
+                placeholder="song"
+                onChange={(val) => songChange(val.target.value)}
+              />
+            </div>
             <hr className={styles["divider"] + " mt-small mb-small"} />
             <div className="group_inputs__group">
               <div className="input__group">
@@ -206,13 +257,56 @@ export default function CreatePage({ session }: { session: Session }) {
                 <label htmlFor="">Music NFT Cover </label>
                 <input
                   className="text_input"
-                  datatype="text"
+                  datatype="file"
+                  type="file"
                   placeholder="image"
                   onChange={(val) => imageChange(val.target.value)}
-                  defaultValue="default image link"
+                  defaultValue={
+                    !image ? "/dist/images/mock/users/3.png" : image
+                  }
                 />
               </div>
             </div>
+            <hr className={styles["divider"] + " mt-small mb-small"} />
+
+            <div className="group_inputs__group group_inputs__group--auction">
+              <div className="input__group input__group--auction">
+                <label> Auction</label>
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    onChange={(val) => typeChange(val.target.checked)}
+                    id="auction"
+                    name="auction"
+                  />
+                  <span className="slider" />
+                </label>
+              </div>
+              <div className="vertical_divider" />
+              {saleType === "fixed" ? (
+                <div className="input__group">
+                  <label htmlFor="">NFT Price </label>
+                  <input
+                    type="number"
+                    placeholder="sale price"
+                    onChange={(val) => salePriceChange(val.target.value)}
+                    className="text_input"
+                  />
+                </div>
+              ) : (
+                <div className="input__group">
+                  <label htmlFor="">Auction reserve price </label>
+                  <input
+                    type="number"
+                    placeholder="auction reserve price"
+                    onChange={(val) => reservePriceChange(val.target.value)}
+                    className="text_input"
+                  />
+                </div>
+              )}
+            </div>
+            <hr className={styles["divider"] + " mt-small mb-small"} />
+
             <div className="input__group">
               <label htmlFor="">NFT Description </label>
               <textarea
@@ -223,36 +317,7 @@ export default function CreatePage({ session }: { session: Session }) {
                 // defaultValue="default description"
               />
             </div>
-            <div className="group_inputs__group">
-              <label> Auction</label>
-              <input
-                type="checkbox"
-                onChange={(val) => typeChange(val.target.checked)}
-                id="auction"
-                name="auction"
-              />
-              {saleType === "fixed" ? (
-                <div className="input__group">
-                  <label htmlFor="">Placeholder </label>
-                  <input
-                    type="number"
-                    placeholder="sale price"
-                    onChange={(val) => salePriceChange(val.target.value)}
-                    className="text_input"
-                  />
-                </div>
-              ) : (
-                <div className="input__group">
-                  <label htmlFor="">Placeholder </label>
-                  <input
-                    type="number"
-                    placeholder="auction reserve price"
-                    onChange={(val) => reservePriceChange(val.target.value)}
-                    className="text_input"
-                  />
-                </div>
-              )}
-            </div>
+
             {/* <hr className={styles["divider"]} /> */}
             <div className="input__group input__group-checkbox mt-small">
               <input
@@ -270,7 +335,7 @@ export default function CreatePage({ session }: { session: Session }) {
             </div>
             <button
               className={
-                styles["primary_button"] + " primary_button mt-small mb-medium"
+                styles["primary_button"] + " primary_button mt-small mb-big"
               }
               onClick={() => createNFTSubmit()}
             >
@@ -283,10 +348,29 @@ export default function CreatePage({ session }: { session: Session }) {
             styles["single__section"] + " " + styles["single__preview"]
           }
         >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
-          laboriosam debitis, corrupti veniam obcaecati optio esse, omnis porro
-          facere delectus ipsum assumenda velit voluptatibus veritatis amet
-          quibusdam dolorum tempora autem!
+          <div className={styles["single__preview-artwork"]}>
+            <div className={styles["single__preview-artwork--image"]}>
+              {renderImage()}
+            </div>
+
+            <div className={styles["single__preview-artwork--content"]}>
+              <div className={styles["single__preview-artwork--player"]}>
+                <PlayerContainer
+                  url={song}
+                  artist="{artwork.creator.handle}"
+                  title="{artwork.title}"
+                />
+                <div className={styles["single__preview-artwork--heading"]}>
+                  <h1>{title}</h1>
+                  <small>
+                    <a href="#">hashtune.co/user-handle/{handle}</a>
+                  </small>
+                </div>
+              </div>
+              <hr className="divider" />
+              <p>{description}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
